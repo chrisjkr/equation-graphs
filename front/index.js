7436a70c7f17d6802141eeaab58c8124f4bdd69f
Krzysztof Kraszewski <krszwsk@gmail.com>
2016-12-03 19:11:52 +0100

    :zap: d3

--- front/index.js
+++ front/index.js
@@ -12,6 +12,13 @@ document.getElementById('linearButton').addEventListener('click', function () {
 
 socket.on('linearResponse', function (data) {
   console.log(data)
+
+  functionPlot({
+    target: '#linear',
+    data: [{
+      fn: 'x^2'
+    }]
+  })
 })
 
 document.getElementById('quadraticButton').addEventListener('click', function () {
