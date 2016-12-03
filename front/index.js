ca4bccf28f817a8851291ddad382decf274c5abc
Krzysztof Kraszewski <krszwsk@gmail.com>
2016-12-03 17:43:26 +0100

    :sparkles: :art: inputs for functions

--- front/index.js
+++ front/index.js
@@ -1,5 +1,14 @@
 var socket = io('http://localhost:3000')
 
+socket.emit('linear', {
+  a: 1,
+  b: 30
+})
+
+socket.on('linearResponse', function (data) {
+  console.log(data)
+})
+
 socket.emit('quadratic', {
   a: 1,
   b: 10,
@@ -7,5 +16,16 @@ socket.emit('quadratic', {
 })
 
 socket.on('quadraticResponse', function (data) {
+  console.log(data)
+})
+
+socket.emit('cubic', {
+  a: 1,
+  b: -4,
+  c: 1,
+  d: 6
+})
+
+socket.on('cubicResponse', function (data) {
   console.log(data)
 })
