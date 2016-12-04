d16384f244f80d532a647e4ae82d473b56c1efb7
Krzysztof Kraszewski <krszwsk@gmail.com>
2016-12-04 18:56:51 +0100

    :sparkles: plotting of quadratic and cubic functions, also not on point

--- front/index.js
+++ front/index.js
@@ -85,6 +85,66 @@ document.getElementById('quadraticButton').addEventListener('click', function ()
 
 socket.on('quadraticResponse', function (data) {
   console.log(data)
+
+  var dataset = []
+  for (var x = -500; x <= 500; x++) {
+    dataset.push({
+      x: x,
+      y: data.a * Math.pow(x, 2) + data.b * x + data.c
+    })
+  }
+  console.log(dataset)
+
+  d3.select('#quadratic').selectAll('svg').remove()
+
+  var svg = d3.select('#quadratic')
+    .append('svg')
+    .attr('width', 500)
+    .attr('height', 400)
+
+  var margin = {top: 20, right: 20, bottom: 30, left: 80}
+  var w = +svg.attr('width') - margin.left - margin.right
+  var h = +svg.attr('height') - margin.top - margin.bottom
+  var g = svg.append('g').attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
+
+  var x = d3.scaleLinear()
+    .rangeRound([0, w])
+  var y = d3.scaleLinear()
+    .rangeRound([h, 0])
+
+  var line = d3.line()
+    .x(function (d) { return x(d.x)})
+    .y(function (d) { return y(d.y) })
+
+  x.domain(d3.extent(dataset, function (d) { return d.x }))
+  y.domain(d3.extent(dataset, function (d) { return d.y }))
+
+  g.append('g')
+    .attr('class', 'axis axis--x')
+    .attr('transform', 'translate(0 ,' + h + ')')
+    .call(d3.axisBottom(x))
+    .append('text')
+    .attr('fill', '#fff')
+    .attr('y', 6)
+    .attr('dy', '0.71em')
+    .style('text-anchor', 'end')
+    .text('X')
+
+  g.append('g')
+    .attr('class', 'axis axis--y')
+    .call(d3.axisLeft(y))
+    .append('text')
+    .attr('fill', '#fff')
+    .attr('transform', 'rotate(-90)')
+    .attr('y', 6)
+    .attr('dy', '0.71em')
+    .style('text-anchor', 'end')
+    .text('Y')
+
+  g.append('path')
+    .datum(dataset)
+    .attr('class', 'line')
+    .attr('d', line)
 })
 
 document.getElementById('cubicButton').addEventListener('click', function () {
@@ -99,4 +159,64 @@ document.getElementById('cubicButton').addEventListener('click', function () {
 
 socket.on('cubicResponse', function (data) {
   console.log(data)
+
+  var dataset = []
+  for (var x = -500; x <= 500; x++) {
+    dataset.push({
+      x: x,
+      y: data.a * Math.pow(x, 3) + data.b * Math.pow(x, 2) + data.c * x + data.d
+    })
+  }
+  console.log(dataset)
+
+  d3.select('#cubic').selectAll('svg').remove()
+
+  var svg = d3.select('#cubic')
+    .append('svg')
+    .attr('width', 500)
+    .attr('height', 400)
+
+  var margin = {top: 20, right: 20, bottom: 30, left: 80}
+  var w = +svg.attr('width') - margin.left - margin.right
+  var h = +svg.attr('height') - margin.top - margin.bottom
+  var g = svg.append('g').attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
+
+  var x = d3.scaleLinear()
+    .rangeRound([0, w])
+  var y = d3.scaleLinear()
+    .rangeRound([h, 0])
+
+  var line = d3.line()
+    .x(function (d) { return x(d.x)})
+    .y(function (d) { return y(d.y) })
+
+  x.domain(d3.extent(dataset, function (d) { return d.x }))
+  y.domain(d3.extent(dataset, function (d) { return d.y }))
+
+  g.append('g')
+    .attr('class', 'axis axis--x')
+    .attr('transform', 'translate(0 ,' + h + ')')
+    .call(d3.axisBottom(x))
+    .append('text')
+    .attr('fill', '#fff')
+    .attr('y', 6)
+    .attr('dy', '0.71em')
+    .style('text-anchor', 'end')
+    .text('X')
+
+  g.append('g')
+    .attr('class', 'axis axis--y')
+    .call(d3.axisLeft(y))
+    .append('text')
+    .attr('fill', '#fff')
+    .attr('transform', 'rotate(-90)')
+    .attr('y', 6)
+    .attr('dy', '0.71em')
+    .style('text-anchor', 'end')
+    .text('Y')
+
+  g.append('path')
+    .datum(dataset)
+    .attr('class', 'line')
+    .attr('d', line)
 })
