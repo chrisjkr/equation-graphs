623da3d3ff7de20a5b865db7c2bd95bd8d5502bd
Krzysztof Kraszewski <krszwsk@gmail.com>
2016-12-06 17:04:30 +0100

    implemented functionPlot

--- front/index.js
+++ front/index.js
@@ -1,4 +1,4 @@
-/* global io, d3 */
+/* global io, functionPlot */
 
 var socket = io('http://localhost:3000')
 
@@ -13,65 +13,12 @@ document.getElementById('linearButton').addEventListener('click', function () {
 socket.on('linearResponse', function (data) {
   console.log(data)
 
-  var dataset = []
-  for (var x = -500; x <= 500; x++) {
-    dataset.push({
-      x: x,
-      y: data.a * x + data.b
-    })
-  }
-  console.log(dataset)
-
-  d3.select('#linear').selectAll('svg').remove()
-
-  var svg = d3.select('#linear')
-    .append('svg')
-    .attr('width', 500)
-    .attr('height', 400)
-
-  var margin = {top: 20, right: 20, bottom: 30, left: 80}
-  var w = +svg.attr('width') - margin.left - margin.right
-  var h = +svg.attr('height') - margin.top - margin.bottom
-  var g = svg.append('g').attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
-
-  var x = d3.scaleLinear()
-    .rangeRound([0, w])
-  var y = d3.scaleLinear()
-    .rangeRound([h, 0])
-
-  var line = d3.line()
-    .x(function (d) { return x(d.x)})
-    .y(function (d) { return y(d.y) })
-
-  x.domain(d3.extent(dataset, function (d) { return d.x }))
-  y.domain(d3.extent(dataset, function (d) { return d.y }))
-
-  g.append('g')
-    .attr('class', 'axis axis--x')
-    .attr('transform', 'translate(0 ,' + h + ')')
-    .call(d3.axisBottom(x))
-    .append('text')
-    .attr('fill', '#fff')
-    .attr('y', 6)
-    .attr('dy', '0.71em')
-    .style('text-anchor', 'end')
-    .text('X')
-
-  g.append('g')
-    .attr('class', 'axis axis--y')
-    .call(d3.axisLeft(y))
-    .append('text')
-    .attr('fill', '#fff')
-    .attr('transform', 'rotate(-90)')
-    .attr('y', 6)
-    .attr('dy', '0.71em')
-    .style('text-anchor', 'end')
-    .text('Y')
-
-  g.append('path')
-    .datum(dataset)
-    .attr('class', 'line')
-    .attr('d', line)
+  functionPlot({
+    target: '#linear',
+    data: [{
+      fn: data.a + 'x + ' + data.b
+    }]
+  })
 })
 
 document.getElementById('quadraticButton').addEventListener('click', function () {
@@ -95,56 +42,6 @@ socket.on('quadraticResponse', function (data) {
   }
   console.log(dataset)
 
-  d3.select('#quadratic').selectAll('svg').remove()
-
-  var svg = d3.select('#quadratic')
-    .append('svg')
-    .attr('width', 500)
-    .attr('height', 400)
-
-  var margin = {top: 20, right: 20, bottom: 30, left: 80}
-  var w = +svg.attr('width') - margin.left - margin.right
-  var h = +svg.attr('height') - margin.top - margin.bottom
-  var g = svg.append('g').attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
-
-  var x = d3.scaleLinear()
-    .rangeRound([0, w])
-  var y = d3.scaleLinear()
-    .rangeRound([h, 0])
-
-  var line = d3.line()
-    .x(function (d) { return x(d.x)})
-    .y(function (d) { return y(d.y) })
-
-  x.domain(d3.extent(dataset, function (d) { return d.x }))
-  y.domain(d3.extent(dataset, function (d) { return d.y }))
-
-  g.append('g')
-    .attr('class', 'axis axis--x')
-    .attr('transform', 'translate(0 ,' + h + ')')
-    .call(d3.axisBottom(x))
-    .append('text')
-    .attr('fill', '#fff')
-    .attr('y', 6)
-    .attr('dy', '0.71em')
-    .style('text-anchor', 'end')
-    .text('X')
-
-  g.append('g')
-    .attr('class', 'axis axis--y')
-    .call(d3.axisLeft(y))
-    .append('text')
-    .attr('fill', '#fff')
-    .attr('transform', 'rotate(-90)')
-    .attr('y', 6)
-    .attr('dy', '0.71em')
-    .style('text-anchor', 'end')
-    .text('Y')
-
-  g.append('path')
-    .datum(dataset)
-    .attr('class', 'line')
-    .attr('d', line)
 })
 
 document.getElementById('cubicButton').addEventListener('click', function () {
@@ -169,54 +66,4 @@ socket.on('cubicResponse', function (data) {
   }
   console.log(dataset)
 
-  d3.select('#cubic').selectAll('svg').remove()
-
-  var svg = d3.select('#cubic')
-    .append('svg')
-    .attr('width', 500)
-    .attr('height', 400)
-
-  var margin = {top: 20, right: 20, bottom: 30, left: 80}
-  var w = +svg.attr('width') - margin.left - margin.right
-  var h = +svg.attr('height') - margin.top - margin.bottom
-  var g = svg.append('g').attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
-
-  var x = d3.scaleLinear()
-    .rangeRound([0, w])
-  var y = d3.scaleLinear()
-    .rangeRound([h, 0])
-
-  var line = d3.line()
-    .x(function (d) { return x(d.x)})
-    .y(function (d) { return y(d.y) })
-
-  x.domain(d3.extent(dataset, function (d) { return d.x }))
-  y.domain(d3.extent(dataset, function (d) { return d.y }))
-
-  g.append('g')
-    .attr('class', 'axis axis--x')
-    .attr('transform', 'translate(0 ,' + h + ')')
-    .call(d3.axisBottom(x))
-    .append('text')
-    .attr('fill', '#fff')
-    .attr('y', 6)
-    .attr('dy', '0.71em')
-    .style('text-anchor', 'end')
-    .text('X')
-
-  g.append('g')
-    .attr('class', 'axis axis--y')
-    .call(d3.axisLeft(y))
-    .append('text')
-    .attr('fill', '#fff')
-    .attr('transform', 'rotate(-90)')
-    .attr('y', 6)
-    .attr('dy', '0.71em')
-    .style('text-anchor', 'end')
-    .text('Y')
-
-  g.append('path')
-    .datum(dataset)
-    .attr('class', 'line')
-    .attr('d', line)
 })
